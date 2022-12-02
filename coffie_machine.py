831402aa5126d8bf7a7409f89ea5f25d65f609b7
Kirtimaanwani <sushilsw111@gmail.com>
2022-12-02 18:14:11 +0530

    updated code

--- coffie_machine.py
+++ coffie_machine.py
@@ -1,28 +1,29 @@
 from data import MENU, resources
-# TODO 1:Prompt user by asking “ What would you like? (espresso/latte/cappuccino)"
-while True:
 
-    coffee_choice = input("What would you like? (espresso/latte/cappuccino): ")
-
-    # TODO 2:Turn off the Coffee Machine by entering “ off ” to the prompt.
-    if coffee_choice.lower() == 'off':
-        break
-
-    def report():
+IsMachineOnline = True
+import time
+class CoffieMachine:
+    
+    def __init__(self, ismachineonline:IsMachineOnline):
+        
+        self.coffee_choice = input("What would you like? (espresso/latte/cappuccino): ")
+        self.running_status = ismachineonline
+        if self.coffee_choice.lower() == 'off':
+            print("Thank you for Visiting")
+            self.running_status = False
+            
+        
+    def report(self):
         """
         Shows all resources available inside coffee machine
         """
         # TODO 3: Print report.
-        if coffee_choice.lower() == 'report':
+        if self.coffee_choice.lower() == 'report':
             for i in resources:
                 print(f"{i} : {resources[i]}")
             return True
-
-
-# TODO 5:Process coins. quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
-
-
-    def coins():
+    
+    def coins(self):
         """
         takes coins from buyer and sum total its value
         """
@@ -32,51 +33,49 @@ while True:
         pennies = 0.01
         coin_list = ["quarters", "dimes", "nickles", "pennies"]
         buyer_coin = [int(input(f"How many {c}? ")) for c in coin_list]
-        coin_list1 = [quarters, dimes, nickles, pennies]
-        buyer_coin_value = [i*j for (i, j) in zip(buyer_coin, coin_list1)]
+        coin_value = [quarters, dimes, nickles, pennies]
+        buyer_coin_value = [i*j for (i, j) in zip(buyer_coin, coin_value)]
         return sum(buyer_coin_value)
-
-# TODO 6:Check transaction successful? “ Sorry that's not enough money. Money refunded. ”. profit “report” is triggered.
-
-    # TODO 7: give change if any remaining.“Here is $2.45 dollars in change.”
-
-    def check_transaction():
+    
+    def check_transaction(self, menu:MENU):
         """
         compares the actual cost of coffee to the sym total of buyer's coin
         """
-        buyer_money = coins()
+        buyer_money = self.coins()
         resources["money"] = 0
-        if buyer_money >= MENU[coffee_choice]["cost"]:
-            resources["money"] += MENU[coffee_choice]["cost"]
-            if buyer_money >= MENU[coffee_choice]["cost"]:
-                change = buyer_money - MENU[coffee_choice]["cost"]
+        if buyer_money >= menu[self.coffee_choice]["cost"]:
+            resources["money"] += menu[self.coffee_choice]["cost"]
+            if buyer_money >= menu[self.coffee_choice]["cost"]:
+                change = buyer_money - menu[self.coffee_choice]["cost"]
                 print(f"Here is ${round(change, 2)} dollars in change")
                 return True
         else:
-            print("Sorry that's not enough money. Money refunded.")
+            print("Sorry that's not enough money. Money refunded.") 
 
-
-    # TODO 8: make coffee, resources should be deducted as per type of coffee ordered
-
-
-    def resource_deduct():
+    
+    def resource_deduct(self, menu:MENU):
         """
         deducts resources for particular coffee making from available resources
         """
-        for supply1 in MENU[coffee_choice]["ingredients"]:
-            resources[supply1] -= MENU[coffee_choice]["ingredients"][supply1]
+        for supply1 in menu[self.coffee_choice]["ingredients"]:
+            resources[supply1] -= menu[self.coffee_choice]["ingredients"][supply1]
+    
 
-    # TODO 9:“Here is your latte. Enjoy!”
-    if report() is True:
+while IsMachineOnline:
+    coffiemachine = CoffieMachine(IsMachineOnline)
+    if not coffiemachine.running_status:
+        break
+    if coffiemachine.report():
         continue
 
-    # TODO 4: Check resources sufficient? “ Sorry there is not enough water. ”
-
-    for supply in MENU[coffee_choice]["ingredients"]:
-        if not MENU[coffee_choice]["ingredients"][supply] < resources[supply]:
+    for supply in MENU[coffiemachine.coffee_choice]["ingredients"]:
+        if not MENU[coffiemachine.coffee_choice]["ingredients"][supply] < resources[supply]:
             print(f"Sorry, there is not enough {supply}. ")
             break
+    
     else:
-        if check_transaction() is True:
-            resource_deduct()
-            print(f"Here is your {coffee_choice}. Enjoy!")
+        if coffiemachine.check_transaction(menu=MENU):
+            coffiemachine.resource_deduct(menu=MENU)
+            print(f"Here is your {coffiemachine.coffee_choice}. Enjoy!")
+            print("Resetting in 5 seconds...\n\n")
+            time.sleep(5)
